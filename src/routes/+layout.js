ccbc3c6d53ca84f1de10a8a2efdafb6adf59699a
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2023-03-14 18:53:34 +1100

    chore: Configure SvelteKit app for Tauri

---
+++ src/routes/+layout.js
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Gonzalo Bulnes Guilpain
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// See https://tauri.app/v1/guides/getting-started/setup/sveltekit#sveltekit-in-ssg-mode
+export const prerender = true;
+export const ssr = false;
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
 // SPDX-FileCopyrightText: 2023 Gonzalo Bulnes Guilpain
 //
-// SPDX-License-Identifier: CC0-1.0
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
