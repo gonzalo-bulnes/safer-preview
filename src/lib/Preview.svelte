6e5756da7021aa273a60dad56475cf6218624cc8
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2023-03-16 02:12:05 +1100

    feat(frontend): Add Preview component

---
+++ src/lib/Preview.svelte
@@ -0,0 +1,61 @@
+<!--
+SPDX-FileCopyrightText: 2023 Gonzalo Bulnes Guilpain
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+<script>
+	import image from './SitaSingsTheBlues.png';
+
+	let revealed = false;
+
+	const onClick = () => {
+		console.log('clicked');
+		revealed = !revealed;
+	};
+</script>
+
+<div class={['preview', revealed ? 'revealed' : ''].join(' ')}>
+	<img alt="" src={image} />
+	<div class="button">
+		<button on:click={onClick}>{revealed ? 'Blur' : 'Reveal'}</button>
+	</div>
+</div>
+
+<style>
+	.preview {
+		display: inline-block;
+		overflow: hidden;
+		min-height: 240px;
+		min-width: 320px;
+		max-height: 100%;
+		max-width: 100%;
+		box-sizing: border-box;
+		background-color: #333;
+	}
+	.preview,
+	img {
+		position: relative;
+	}
+	img {
+		filter: blur(8px);
+	}
+
+	.revealed img {
+		filter: blur(0);
+	}
+
+	.button {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: absolute;
+		top: 0;
+		left: 0;
+		box-sizing: border-box;
+	}
+	.revealed .button {
+		border-width: 0;
+	}
+</style>
---
+++ src/stories/Preview.stories.js
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2023 Gonzalo Bulnes Guilpain
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import Preview from '../lib/Preview.svelte';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/svelte/writing-stories/introduction
+export default {
+	title: 'Composite Components/Preview',
+	component: Preview,
+	tags: ['autodocs'],
+	argTypes: {}
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/svelte/writing-stories/args
+export const Default = {
+	args: {}
+};
