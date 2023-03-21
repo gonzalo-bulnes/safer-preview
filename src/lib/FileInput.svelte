02d4881f0c5fb2d5be65cabaadb239e8c0689d92
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2023-03-22 05:34:15 +1100

    feat(frontend): Add FileInput component

---
+++ src/lib/FileInput.svelte
@@ -0,0 +1,42 @@
+<!--
+SPDX-FileCopyrightText: 2023 Gonzalo Bulnes Guilpain
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+<script>
+	/**
+	 * A file input that allows files to be opened.
+	 *
+	 * This minimal form relies on the native file dialog
+	 * to allow the filesystem to be browsed and one file
+	 * file to be selected.
+	 *
+	 * @component
+	 */
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	/**
+	 * Overriding style. Use wisely.
+	 * @type {string | undefined}
+	 */
+	export let style = undefined;
+
+	/** @type {FileList | undefined} */
+	let files;
+
+	/**
+	 * The file that is currently selected
+	 * @type {File | null } */
+	let selected = null;
+	$: selected = files && files.length > 0 ? files.item(0) : null;
+
+	const emitFileSelected = () => {
+		dispatch('fileSelected', {
+			file: selected
+		});
+	};
+</script>
+
+<input type="file" {style} bind:files on:change={emitFileSelected} />
