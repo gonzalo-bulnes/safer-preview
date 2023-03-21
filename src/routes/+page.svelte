73e7c99b1c9fa0e4de61f023507f1a8333e887f2
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2023-03-22 05:41:08 +1100

    feat(frontend): Add option to select image to preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@ SPDX-FileCopyrightText: 2023 Gonzalo Bulnes Guilpain
 SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <script>
-	import Preview from '../lib/Preview.svelte';
+	import MainPage from '../lib/MainPage.svelte';
 </script>
 
-<Preview />
+<MainPage />
