1f2dafd7ce2b8ec9c89b3ef4b386b3b827dc4f38
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2023-03-22 06:38:58 +1100

    feat(frontend): Add Button component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,30 @@
+<!--
+SPDX-FileCopyrightText: 2023 Gonzalo Bulnes Guilpain
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+<script>
+	/**
+	 * A button.
+	 *
+	 * The button text can be set, or content passed as a slot.
+	 *
+	 * @component
+	 */
+
+	/**
+	 * Overriding style. Use wisely.
+	 * @type {string | undefined}
+	 */
+	export let style = undefined;
+
+	/**
+	 * The button's text
+	 * @type {string|undefined}
+	 */
+	export let text = undefined;
+</script>
+
+<button on:click {style}>
+	<slot>{text ? text : 'Click me!'}</slot>
+</button>
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,47 +1,31 @@
 // SPDX-FileCopyrightText: 2023 Gonzalo Bulnes Guilpain
 //
-// SPDX-License-Identifier: CC0-1.0
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
-import Button from './Button.svelte';
+import Button from '../lib/Button.svelte';
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/svelte/writing-stories/introduction
 export default {
-	title: 'Example/Button',
+	title: 'Atomic Components/Button',
 	component: Button,
 	tags: ['autodocs'],
 	argTypes: {
-		backgroundColor: { control: 'color' },
-		size: {
-			control: { type: 'select' },
-			options: ['small', 'medium', 'large']
+		style: {
+			control: { type: 'text' }
+		},
+		text: {
+			control: { type: 'text' }
 		}
 	}
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/7.0/svelte/writing-stories/args
-export const Primary = {
-	args: {
-		primary: true,
-		label: 'Button'
-	}
+export const Default = {
+	args: {}
 };
 
-export const Secondary = {
+export const Text = {
 	args: {
-		label: 'Button'
-	}
-};
-
-export const Large = {
-	args: {
-		size: 'large',
-		label: 'Button'
-	}
-};
-
-export const Small = {
-	args: {
-		size: 'small',
-		label: 'Button'
+		text: 'Reveal'
 	}
 };
